e657931409bcbcb81abbf3d3e001c15141755fa3
Adam Moody <moody20@llnl.gov>
2018-03-30 13:28:50 -0700

    improve documentation of redset interface

--- src/redset.h
+++ src/redset.h
@@ -78,56 +78,63 @@ int redset_finalize();
 
 /* create a new redundancy set descriptor */
 int redset_create(
-  int type,          /* redundancy encoding type */
-  MPI_Comm comm,     /* process group participating in set */
-  const char* group, /* string specifying procs in the same failure group */
-  redset* d          /* output redundancy descriptor */
+  int type,          /* IN  - redundancy encoding type: one of REDSET_COPY values */
+  MPI_Comm comm,     /* IN  - process group participating in set */
+  const char* group, /* IN  - string specifying procs in the same failure group */
+  redset* d          /* OUT - output redundancy descriptor */
 );
 
 /* free any memory associated with the specified redundancy descriptor */
 int redset_delete(
-  redset* d
+  redset* d /* INOUT - redundancy descriptor to be freed */
 );
 
 /* apply redundancy scheme to file and return number of bytes copied
  * in bytes parameter */
 int redset_apply(
-  int numfiles,
-  const char** files,
-  const char* name,
-  const redset* d
+  int numfiles,       /* IN - number of file names in files array */
+  const char** files, /* IN - list of file names of length numfiles */
+  const char* name,   /* IN - path/filename prefix to prepend to redset metadata files */
+  const redset* d     /* IN - redundancy decriptor to be applied */
 );
 
 /* rebuilds files for specified dataset id using specified redundancy descriptor,
  * adds them to filemap, and returns REDSET_SUCCESS if all processes succeeded */
 int redset_recover(
-  MPI_Comm comm,
-  const char* name,
-  redset* d
+  MPI_Comm comm,    /* IN  - parent communicator containg set of processes rebuilding data */
+  const char* name, /* IN  - path/filename prefix to prepend to redset metadata files */
+  redset* d         /* OUT - output redundancy descriptor */
 );
 
 /* deletes redundancy data that was added in redset_apply,
  * which is useful when cleaning up */
 int redset_unapply(
-  const char* name,
-  redset* d
+  const char* name, /* IN - path/filename prefix to prepend to redset metadata files */
+  redset* d         /* IN - redundancy descriptor associated with above path */
 );
 
 /* return list of files added by redundancy scheme */
 redset_filelist* redset_filelist_get(
-  const char* name,
-  redset* d
+  const char* name, /* IN - path/filename prefix to prepend to redset metadata files */
+  redset* d         /* IN - redundancy descriptor associated with above path */
 );
 
-/* free file list allocated by call to redset_filelist */
-int redset_filelist_release(redset_filelist** plist);
+/* free file list allocated by call to redset_filelist_get */
+int redset_filelist_release(
+  redset_filelist** plist /* INOUT - address of pointer to list to be freed, sets pointer to NULL */
+);
 
 /* return number of files in file list */
-int redset_filelist_count(redset_filelist* list);
+int redset_filelist_count(
+  redset_filelist* list /* IN - list of redundancy files */
+);
 
 /* return name of file at specified index, where
  * 0 <= index < count and count is value returned by redset_filelist_count */
-const char* redset_filelist_file(redset_filelist* list, int index);
+const char* redset_filelist_file(
+  redset_filelist* list, /* IN - list of redundancy files */
+  int index              /* IN - index into list, ranges from 0 to list count - 1 */
+);
 
 #define ER_SUCCESS (0)
 #define ER_FAILURE (1)
@@ -135,7 +142,9 @@ const char* redset_filelist_file(redset_filelist* list, int index);
 #define ER_DIRECTION_ENCODE  (1)
 #define ER_DIRECTION_REBUILD (2)
 
-int ER_Init(const char* conf_file);
+int ER_Init(
+  const char* conf_file
+);
 
 int ER_Finalize();
 
