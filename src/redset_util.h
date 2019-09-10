a574ae07b55035ffecd97f6be3be11bc92d81c89
Elsa Gonsiorowski (Uranus) <gonsiorowski1@llnl.gov>
2019-09-09 17:17:11 -0700

    doxygen-style comments

--- src/redset_util.h
+++ src/redset_util.h
@@ -14,34 +14,38 @@ extern char* redset_hostname;
 extern int redset_mpi_buf_size;
 extern size_t redset_page_size;
 
-/* print error message to stdout */
+/** \file redset_util.h
+ *  \ingroup redset
+ *  \brief utilities for redundancy sets */
+
+/** print error message to stdout */
 void redset_err(const char *fmt, ...);
 
-/* print warning message to stdout */
+/** print warning message to stdout */
 void redset_warn(const char *fmt, ...);
 
-/* print message to stdout if redset_debug is set and it is >= level */
+/** print message to stdout if redset_debug is set and it is >= level */
 void redset_dbg(int level, const char *fmt, ...);
 
-/* print abort message and kill run */
+/** print abort message and kill run */
 void redset_abort(int rc, const char *fmt, ...);
 
-/* allocate size bytes, returns NULL if size == 0,
+/** allocate size bytes, returns NULL if size == 0,
  * calls redset_abort if allocation fails */
 #define REDSET_MALLOC(X) redset_malloc(X, __FILE__, __LINE__);
 void* redset_malloc(size_t size, const char* file, int line);
 
-/* pass address of pointer to be freed, frees memory if not NULL and sets pointer to NULL */
+/** pass address of pointer to be freed, frees memory if not NULL and sets pointer to NULL */
 void redset_free(void* ptr);
 
-/* allocates a block of memory and aligns it to specified alignment */
+/** allocates a block of memory and aligns it to specified alignment */
 void* redset_align_malloc(size_t size, size_t align);
 
-/* frees a blocked allocated with a call to redset_align_malloc */
+/** frees a blocked allocated with a call to redset_align_malloc */
 void redset_align_free(void* buf);
 
-/* sends a NUL-terminated string to a process,
- * allocates space and recieves a NUL-terminated string from a process,
+/** sends a NUL-terminated string to a process,
+ * allocates space and recieves a NULL-terminated string from a process,
  * can specify MPI_PROC_NULL as either send or recv rank */
 int redset_str_sendrecv(
   const char* send_str, int send_rank,
@@ -49,7 +53,7 @@ int redset_str_sendrecv(
   MPI_Comm comm
 );
 
-/* returns true (non-zero) if flag on each process in comm is true */
+/** returns true (non-zero) if flag on each process in comm is true */
 int redset_alltrue(int flag, MPI_Comm comm);
 
 #endif
