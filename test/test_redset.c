3d5e995d147da651f70d9e90cccfb12eb6bf9396
Adam Moody <moody20@llnl.gov>
2020-07-26 17:01:24 -0700

    test: check loss of all combinations of ranks for given k

--- test/test_redset.c
+++ test/test_redset.c
@@ -427,7 +427,7 @@ int test_recover_loss_two_ranks(int mode, const char* path, int count, const cha
       printf("mode = %d: Recover time (two ranks): %f\n", mode, (end - start));
     }
 
-    if (mode != REDSET_COPY_RS) {
+    if (mode == REDSET_COPY_SINGLE || mode == REDSET_COPY_XOR) {
       if (recovered) {
         printf("ERROR: should not have be able to recover files\n");
         rc = 1;
@@ -470,7 +470,7 @@ int test_recover_loss_two_ranks(int mode, const char* path, int count, const cha
     redset_rc = redset_recover(comm, path, &d);
     recovered = alltrue(redset_rc == REDSET_SUCCESS, comm);
   
-    if (mode != REDSET_COPY_RS) {
+    if (mode == REDSET_COPY_SINGLE || mode == REDSET_COPY_XOR) {
       if (recovered) {
         printf("ERROR: should not have be able to recover files\n");
         rc = 1;
@@ -505,6 +505,176 @@ int test_recover_loss_two_ranks(int mode, const char* path, int count, const cha
   return rc;
 }
 
+int increment_index(int* index, int i, int k, int ranks)
+{
+  /* compute limit of current index */
+  int limit = ranks - (k - i);
+  if (index[i] < limit) {
+    /* current index is below its limit, bump it up */
+    index[i]++;
+    return 0;
+  } else if (i > 0) {
+    /* current index has reached its limit,
+     * try to bump the next index down if there is one */
+    increment_index(index, i - 1, k, ranks);
+
+    /* reset current index to one more than index below */
+    index[i] = index[i - 1] + 1;
+  } else {
+    /* we've exhausted the lowest index value,
+     * bump it up anyway so higher index values
+     * also exceed their limits */
+    index[i]++;
+  }
+
+  if (index[i] > limit) {
+    /* we've exhausted the range for this value */
+    return 1;
+  }
+
+  /* otherwise, we found a valid index value */
+  return 0;
+}
+
+/* test that recover succeeds when no files are lost,
+ * nothing to do, but should still return success */
+int test_recover_loss_k_ranks(int mode, const char* path, int count, const char** filelist, uLong* crcvals, int k, MPI_Comm comm)
+{
+  int rc = 0;
+
+  int rank, ranks;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &ranks);
+
+  int* index = (int*) malloc(k * sizeof(int));
+
+  /* initialize index values to 0, 1, ..., k-1 */
+  int i;
+  for (i = 0; i < k; i++) {
+    index[i] = i;
+  }
+
+  int done = 0;
+  while (! done) {
+    /* delete files on rank 0 */
+    if (rank == 0) {
+      printf("mode = %d: deleting ranks = ", mode); 
+      for (i = 0; i < k; i++) {
+        printf("%d, ", index[i]); 
+      }
+      printf("\n");
+      fflush(stdout);
+    }
+    for (i = 0; i < k; i++) {
+      if (rank == index[i]) {
+        delete_files(count, filelist);
+      }
+    }
+  
+    double start = MPI_Wtime();
+
+    redset d;
+    int redset_rc = redset_recover(comm, path, &d);
+    int recovered = alltrue(redset_rc == REDSET_SUCCESS, comm);
+  
+    double end = MPI_Wtime();
+    if (rank == 0) {
+      printf("mode = %d: Recover time (two ranks): %f\n", mode, (end - start));
+    }
+
+    if ((mode == REDSET_COPY_SINGLE && k > 0) ||
+        (mode == REDSET_COPY_XOR    && k > 1))
+     {
+      if (recovered) {
+        printf("ERROR: should not have be able to recover files\n");
+        rc = 1;
+      }
+    } else {
+      if (recovered) {
+        rc = check_crcs(count, filelist, crcvals);
+      } else {
+        printf("ERROR: recover failed\n");
+        rc = 1;
+      }
+  
+      int tmp_rc = check_for_redundancy_files(mode, path, d);
+      if (tmp_rc) {
+        rc = tmp_rc;
+      }
+    }
+  
+    if (rc == 0 && recovered) {
+      /* delete files on rank 0 */
+      if (rank == 0) {
+        printf("mode = %d: deleting ranks and redundancy = ", mode); 
+        for (i = 0; i < k; i++) {
+          printf("%d, ", index[i]); 
+        }
+        printf("\n");
+        fflush(stdout);
+      }
+      for (i = 0; i < k; i++) {
+        if (rank == index[i]) {
+          delete_files(count, filelist);
+          delete_redundancy_files(mode, path, d);
+        }
+      }
+    }
+
+    if (redset_delete(&d) != REDSET_SUCCESS) {
+      printf("ERROR: failed to delete redundancy descriptor\n");
+      rc = 1;
+    }
+
+    /* current rank failed to recover, so no need to test other ranks */
+    if (rc || !recovered) {
+      fflush(stdout);
+      return 1;
+    }
+
+    redset_rc = redset_recover(comm, path, &d);
+    recovered = alltrue(redset_rc == REDSET_SUCCESS, comm);
+  
+    if ((mode == REDSET_COPY_SINGLE && k > 0) ||
+        (mode == REDSET_COPY_XOR    && k > 1))
+    {
+      if (recovered) {
+        printf("ERROR: should not have be able to recover files\n");
+        rc = 1;
+      }
+    } else {
+      if (recovered) {
+        rc = check_crcs(count, filelist, crcvals);
+      } else {
+        printf("ERROR: recover failed\n");
+        rc = 1;
+      }
+  
+      int tmp_rc = check_for_redundancy_files(mode, path, d);
+      if (tmp_rc) {
+        rc = tmp_rc;
+      }
+    }
+  
+    if (redset_delete(&d) != REDSET_SUCCESS) {
+      printf("ERROR: failed to delete redundancy descriptor\n");
+      rc = 1;
+    }
+
+    /* current rank failed to recover, so no need to test other ranks */
+    if (rc || !recovered) {
+      fflush(stdout);
+      return 1;
+    }
+
+    done = increment_index(index, k-1, k, ranks);
+  }
+
+  free(index);
+
+  return rc;
+}
+
 void test_sequence(int copymode, const char* group, int filecount, const char** filelist, const char* prefix)
 {
   create_files(filecount, filelist);
@@ -513,7 +683,21 @@ void test_sequence(int copymode, const char* group, int filecount, const char** 
   init_crcs(filecount, filelist, crcvals);
 
   redset d;
-  redset_create(copymode, MPI_COMM_WORLD, group, &d);
+  //redset_create(copymode, MPI_COMM_WORLD, group, &d);
+  switch (copymode) {
+  case REDSET_COPY_SINGLE:
+    redset_new_single(MPI_COMM_WORLD, group, &d);
+    break;
+  case REDSET_COPY_PARTNER:
+    redset_new_partner(MPI_COMM_WORLD, group, 2, &d);
+    break;
+  case REDSET_COPY_XOR:
+    redset_new_xor(MPI_COMM_WORLD, group, 8, &d);
+    break;
+  case REDSET_COPY_RS:
+    redset_new_rs(MPI_COMM_WORLD, group, 8, 2, &d);
+    break;
+  }
 
   test_apply(copymode, filecount, filelist, prefix, d, MPI_COMM_WORLD);
   check_crcs(filecount, filelist, crcvals);
@@ -531,21 +715,42 @@ void test_sequence(int copymode, const char* group, int filecount, const char** 
 
 
 
-  create_files(filecount, filelist);
-  init_crcs(filecount, filelist, crcvals);
+  int ranks;
+  MPI_Comm_size(MPI_COMM_WORLD, &ranks);
 
-  redset_create(copymode, MPI_COMM_WORLD, group, &d);
+  int k;
+  for (k = 2; k < ranks; k++) {
+    create_files(filecount, filelist);
+    init_crcs(filecount, filelist, crcvals);
 
-  test_apply(copymode, filecount, filelist, prefix, d, MPI_COMM_WORLD);
-  check_crcs(filecount, filelist, crcvals);
+    //redset_create(copymode, MPI_COMM_WORLD, group, &d);
+    switch (copymode) {
+    case REDSET_COPY_SINGLE:
+      redset_new_single(MPI_COMM_WORLD, group, &d);
+      break;
+    case REDSET_COPY_PARTNER:
+      redset_new_partner(MPI_COMM_WORLD, group, k, &d);
+      break;
+    case REDSET_COPY_XOR:
+      redset_new_xor(MPI_COMM_WORLD, group, 8, &d);
+      break;
+    case REDSET_COPY_RS:
+      redset_new_rs(MPI_COMM_WORLD, group, 8, k, &d);
+      break;
+    }
 
-  test_recover_loss_two_ranks(copymode, prefix, filecount, filelist, crcvals, MPI_COMM_WORLD);
+    test_apply(copymode, filecount, filelist, prefix, d, MPI_COMM_WORLD);
+    check_crcs(filecount, filelist, crcvals);
 
-  test_unapply(copymode, prefix, d, MPI_COMM_WORLD);
+    //test_recover_loss_two_ranks(copymode, prefix, filecount, filelist, crcvals, MPI_COMM_WORLD);
+    test_recover_loss_k_ranks(copymode, prefix, filecount, filelist, crcvals, k, MPI_COMM_WORLD);
 
-  redset_delete(&d);
+    test_unapply(copymode, prefix, d, MPI_COMM_WORLD);
 
-  delete_files(filecount, filelist);
+    redset_delete(&d);
+
+    delete_files(filecount, filelist);
+  }
 
   free(crcvals);
 
