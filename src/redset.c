dd582f1ef259a5a45bf55c2f3f75b79fdce7896f
Adam Moody <moody20@llnl.gov>
2020-07-28 15:21:50 -0700

    rename interfaces from new to create

--- src/redset.c
+++ src/redset.c
@@ -415,16 +415,25 @@ static int redset_create_base(
   MPI_Comm_rank(d->comm, &d->rank);
   MPI_Comm_size(d->comm, &d->ranks);
 
+#if 0
   /* for our group id, use the global rank of the rank 0 task
    * in our reddesc comm */
   int comm_rank;
   MPI_Comm_rank(comm, &comm_rank);
   d->group_id = comm_rank;
   MPI_Bcast(&d->group_id, 1, MPI_INT, 0, d->comm);
+#endif
 
   /* count the number of groups */
   int group_leader = (d->rank == 0) ? 1 : 0;
   MPI_Allreduce(&group_leader, &d->groups, 1, MPI_INT, MPI_SUM, comm);
+
+  /* assign group ids, execute a scan which will increment
+   * a running count each time a group leader is found,
+   * then bcast the group id from leader to all ranks in its group */
+  MPI_Scan(&group_leader, &d->group_id, 1, MPI_INT, MPI_SUM, comm);
+  MPI_Bcast(&d->group_id, 1, MPI_INT, 0, d->comm);
+  d->group_id--;
 
   /* fill in state struct depending on copy type */
   switch (d->type) {
@@ -731,190 +740,6 @@ int redset_bool_have_files(
 
   /* if we make it here, we have all of our files */
   return 1;
-}
-
-static void redset_stat_get_atimes(const struct stat* sb, uint64_t* secs, uint64_t* nsecs)
-{
-    *secs = (uint64_t) sb->st_atime;
-
-#if HAVE_STRUCT_STAT_ST_MTIMESPEC_TV_NSEC
-    *nsecs = (uint64_t) sb->st_atimespec.tv_nsec;
-#elif HAVE_STRUCT_STAT_ST_MTIM_TV_NSEC
-    *nsecs = (uint64_t) sb->st_atim.tv_nsec;
-#elif HAVE_STRUCT_STAT_ST_MTIME_N
-    *nsecs = (uint64_t) sb->st_atime_n;
-#elif HAVE_STRUCT_STAT_ST_UMTIME
-    *nsecs = (uint64_t) sb->st_uatime * 1000;
-#elif HAVE_STRUCT_STAT_ST_MTIME_USEC
-    *nsecs = (uint64_t) sb->st_atime_usec * 1000;
-#else
-    *nsecs = 0;
-#endif
-}
-
-static void redset_stat_get_mtimes (const struct stat* sb, uint64_t* secs, uint64_t* nsecs)
-{
-    *secs = (uint64_t) sb->st_mtime;
-
-#if HAVE_STRUCT_STAT_ST_MTIMESPEC_TV_NSEC
-    *nsecs = (uint64_t) sb->st_mtimespec.tv_nsec;
-#elif HAVE_STRUCT_STAT_ST_MTIM_TV_NSEC
-    *nsecs = (uint64_t) sb->st_mtim.tv_nsec;
-#elif HAVE_STRUCT_STAT_ST_MTIME_N
-    *nsecs = (uint64_t) sb->st_mtime_n;
-#elif HAVE_STRUCT_STAT_ST_UMTIME
-    *nsecs = (uint64_t) sb->st_umtime * 1000;
-#elif HAVE_STRUCT_STAT_ST_MTIME_USEC
-    *nsecs = (uint64_t) sb->st_mtime_usec * 1000;
-#else
-    *nsecs = 0;
-#endif
-}
-
-static void redset_stat_get_ctimes (const struct stat* sb, uint64_t* secs, uint64_t* nsecs)
-{
-    *secs = (uint64_t) sb->st_ctime;
-
-#if HAVE_STRUCT_STAT_ST_MTIMESPEC_TV_NSEC
-    *nsecs = (uint64_t) sb->st_ctimespec.tv_nsec;
-#elif HAVE_STRUCT_STAT_ST_MTIM_TV_NSEC
-    *nsecs = (uint64_t) sb->st_ctim.tv_nsec;
-#elif HAVE_STRUCT_STAT_ST_MTIME_N
-    *nsecs = (uint64_t) sb->st_ctime_n;
-#elif HAVE_STRUCT_STAT_ST_UMTIME
-    *nsecs = (uint64_t) sb->st_uctime * 1000;
-#elif HAVE_STRUCT_STAT_ST_MTIME_USEC
-    *nsecs = (uint64_t) sb->st_ctime_usec * 1000;
-#else
-    *nsecs = 0;
-#endif
-}
-
-int redset_meta_encode(const char* file, kvtree* meta)
-{
-  struct stat statbuf;
-  int rc = redset_stat(file, &statbuf);
-  if (rc == 0) {
-    kvtree_util_set_unsigned_long(meta, "MODE", (unsigned long) statbuf.st_mode);
-    kvtree_util_set_unsigned_long(meta, "UID",  (unsigned long) statbuf.st_uid);
-    kvtree_util_set_unsigned_long(meta, "GID",  (unsigned long) statbuf.st_gid);
-    kvtree_util_set_unsigned_long(meta, "SIZE", (unsigned long) statbuf.st_size);
-
-    uint64_t secs, nsecs;
-    redset_stat_get_atimes(&statbuf, &secs, &nsecs);
-    kvtree_util_set_unsigned_long(meta, "ATIME_SECS",  (unsigned long) secs);
-    kvtree_util_set_unsigned_long(meta, "ATIME_NSECS", (unsigned long) nsecs);
-
-    redset_stat_get_ctimes(&statbuf, &secs, &nsecs);
-    kvtree_util_set_unsigned_long(meta, "CTIME_SECS",  (unsigned long) secs);
-    kvtree_util_set_unsigned_long(meta, "CTIME_NSECS", (unsigned long) nsecs);
-
-    redset_stat_get_mtimes(&statbuf, &secs, &nsecs);
-    kvtree_util_set_unsigned_long(meta, "MTIME_SECS",  (unsigned long) secs);
-    kvtree_util_set_unsigned_long(meta, "MTIME_NSECS", (unsigned long) nsecs);
-
-    return REDSET_SUCCESS;
-  }
-  return REDSET_FAILURE;
-}
-
-int redset_meta_apply(const char* file, const kvtree* meta)
-{
-  int rc = REDSET_SUCCESS;
-
-  /* set permission bits on file */
-  unsigned long mode_val;
-  if (kvtree_util_get_unsigned_long(meta, "MODE", &mode_val) == KVTREE_SUCCESS) {
-    mode_t mode = (mode_t) mode_val;
-
-    /* TODO: mask some bits here */
-
-    int chmod_rc = chmod(file, mode);
-    if (chmod_rc != 0) {
-      /* failed to set permissions */
-      redset_err("chmod(%s) failed: errno=%d %s @ %s:%d",
-        file, errno, strerror(errno), __FILE__, __LINE__
-      );
-      rc = REDSET_FAILURE;
-    }
-  }
-
-  /* set uid and gid on file */
-  unsigned long uid_val = -1;
-  unsigned long gid_val = -1;
-  kvtree_util_get_unsigned_long(meta, "UID", &uid_val);
-  kvtree_util_get_unsigned_long(meta, "GID", &gid_val);
-  if (uid_val != -1 || gid_val != -1) {
-    /* got a uid or gid value, try to set them */
-    int chown_rc = chown(file, (uid_t) uid_val, (gid_t) gid_val);
-    if (chown_rc != 0) {
-      /* failed to set uid and gid */
-      redset_err("chown(%s, %lu, %lu) failed: errno=%d %s @ %s:%d",
-        file, uid_val, gid_val, errno, strerror(errno), __FILE__, __LINE__
-      );
-      rc = REDSET_FAILURE;
-    }
-  }
-
-  /* can't set the size at this point, but we can check it */
-  unsigned long size;
-  if (kvtree_util_get_unsigned_long(meta, "SIZE", &size) == KVTREE_SUCCESS) {
-    /* got a size field in the metadata, stat the file */
-    struct stat statbuf;
-    int stat_rc = redset_stat(file, &statbuf);
-    if (stat_rc == 0) {
-      /* stat succeeded, check that sizes match */
-      if (size != statbuf.st_size) {
-        /* file size is not correct */
-        redset_err("file `%s' size is %lu expected %lu @ %s:%d",
-          file, (unsigned long) statbuf.st_size, size, __FILE__, __LINE__
-        );
-        rc = REDSET_FAILURE;
-      }
-    } else {
-      /* failed to stat file */
-      redset_err("stat(%s) failed: errno=%d %s @ %s:%d",
-        file, errno, strerror(errno), __FILE__, __LINE__
-      );
-      rc = REDSET_FAILURE;
-    }
-  }
-
-  /* set timestamps on file as last step */
-  unsigned long atime_secs  = 0;
-  unsigned long atime_nsecs = 0;
-  kvtree_util_get_unsigned_long(meta, "ATIME_SECS",  &atime_secs);
-  kvtree_util_get_unsigned_long(meta, "ATIME_NSECS", &atime_nsecs);
-
-  unsigned long mtime_secs  = 0;
-  unsigned long mtime_nsecs = 0;
-  kvtree_util_get_unsigned_long(meta, "MTIME_SECS",  &mtime_secs);
-  kvtree_util_get_unsigned_long(meta, "MTIME_NSECS", &mtime_nsecs);
-
-  if (atime_secs != 0 || atime_nsecs != 0 ||
-      mtime_secs != 0 || mtime_nsecs != 0)
-  {
-    /* fill in time structures */
-    struct timespec times[2];
-    times[0].tv_sec  = (time_t) atime_secs;
-    times[0].tv_nsec = (long)   atime_nsecs;
-    times[1].tv_sec  = (time_t) mtime_secs;
-    times[1].tv_nsec = (long)   mtime_nsecs;
-
-    /* set times with nanosecond precision using utimensat,
-     * assume path is relative to current working directory,
-     * if it's not absolute, and set times on link (not target file)
-     * if dest_path refers to a link */
-    int utime_rc = utimensat(AT_FDCWD, file, times, AT_SYMLINK_NOFOLLOW);
-    if (utime_rc != 0) {
-      redset_err("Failed to change timestamps on `%s' utimensat() errno=%d %s @ %s:%d",
-        file, errno, strerror(errno), __FILE__, __LINE__
-      );
-      rc = REDSET_FAILURE;
-    }
-  }
-
-  return rc;
 }
 
 static int redset_build_filename(
@@ -1377,65 +1202,4 @@ redset_filelist redset_filelist_get(
   list->files = files;
 
   return list;
-}
-
-int redset_filelist_release(redset_filelist* plist)
-{
-  if (plist == NULL) {
-    return REDSET_SUCCESS;
-  }
-
-  redset_list* list = (redset_list*) *plist;
-
-  /* check that we got a list */
-  if (list == NULL) {
-    return REDSET_SUCCESS;
-  }
-
-  /* free each file name string */
-  int i;
-  for (i = 0; i < list->count; i++) {
-    redset_free(&list->files[i]);
-  }
-
-  /* free the list of files itself */
-  redset_free(&list->files);
-
-  /* free the object */
-  redset_free(plist);
-
-  return REDSET_SUCCESS;
-}
-
-/* returns the number of files in the list */
-int redset_filelist_count(redset_filelist listvp)
-{
-  redset_list* list = (redset_list*) listvp;
-
-  /* check that we got a list */
-  if (list == NULL) {
-    return 0;
-  }
-
-  return list->count;
-}
-
-/* returns the name of the file by the given index,
- * index should be between 0 and count-1,
- * returns NULL if index is invalid */
-const char* redset_filelist_file(redset_filelist listvp, int index)
-{
-  redset_list* list = (redset_list*) listvp;
-
-  /* check that we got a list */
-  if (list == NULL) {
-    return NULL;
-  }
-
-  /* check that index is in range */
-  if (index < 0 || index >= list->count) {
-    return NULL;
-  }
-
-  return list->files[index];
 }
