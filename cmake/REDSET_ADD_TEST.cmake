a7c23e40643a1230aab394717b58358b3c5d4177
Gregory A. Kosinovsky <kosinov@quartz2300.llnl.gov>
2020-07-28 15:37:57 -0700

    Fixed LSF

---
+++ cmake/REDSET_ADD_TEST.cmake
@@ -0,0 +1,13 @@
+function(REDSET_ADD_TEST name args outputs)
+
+  # job launcher
+  if(${VELOC_RESOURCE_MANAGER} STREQUAL "LSF")
+    set(test_param jsrun -r 1)
+  elseif(${VELOC_RESOURCE_MANAGER} STREQUAL "SLURM")
+    set(test_param srun -N 3 -n 6)
+  endif()
+
+  # Tests
+  add_test(NAME serial_${name}_start COMMAND ${test_param} ./${name} ${args} )
+
+endfunction(REDSET_ADD_TEST)
